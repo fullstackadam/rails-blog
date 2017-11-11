207a7f482e772df0f7eb99a2adcde671c33d1173
Adam McMahon <mcmahonempire@gmail.com>
2017-11-10 18:06:47 -0700

    add: user page; create partial for articles list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80, center: false })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    center = options[:center]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+
+    classes = 'img-circle img-responsive'
+    classes += ' center-block' if center
+
+    image_tag(gravatar_url, alt: user.username, class: classes)
+  end
 end
